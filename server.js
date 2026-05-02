ea5947aef1321f89404c948eea41fd07c94e1263
privatesolutions35-web <privatesolutions35@gmail.com>
2026-05-02 21:30:09 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -198,6 +198,10 @@ function broadcastOnlineCount() {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
